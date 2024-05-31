4710a001b70a3b83d3029515dcb6daeb0870509c
Eyram-Atsu <wilsonnana1989@gmail.com>
2024-05-31 20:14:45 +0100

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ df=pd.read_csv('superstore.csv',encoding='latin1')
 # get a list of products
 product_list=df['Product Name'].unique()
 
-total_sales_product=df[['Product Name','Sales']].groupby('Product Name').agg({'Sales':'sum'}).reset_index().sort_values(by='Sales', ascending=False)
+total_sales_product=df[['Product Name','Sales']]. \
+    groupby('Product Name').agg({'Sales':'sum'}). \
+        reset_index().sort_values(by='Sales', ascending=False)
 total_sales_product['Sales']=round(total_sales_product['Sales'],2)
 total_sales_product=formatIndex(total_sales_product)
 st.dataframe(total_sales_product.head(10))
