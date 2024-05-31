213317d14b8152470986cdb68e036e5d86f87507
Eyram-Atsu <wilsonnana1989@gmail.com>
2024-05-31 20:04:40 +0100

    app.py

--- app.py
+++ app.py
@@ -12,12 +12,10 @@ df=pd.read_csv('superstore.csv',encoding='latin1')
 # get a list of products
 product_list=df['Product Name'].unique()
 
-total_sales_product=df[['Product Name','Sales']]. \
-    groupby('Product Name').agg({'Sales':'sum'}). \
-        reset_index().sort_values(by='Sales', ascending=False)
+total_sales_product=df[['Product Name','Sales']].groupby('Product Name').agg({'Sales':'sum'}).reset_index().sort_values(by='Sales', ascending=False)
 total_sales_product['Sales']=round(total_sales_product['Sales'],2)
 total_sales_product=formatIndex(total_sales_product)
 st.dataframe(total_sales_product.head(10))
 
-fig=px.bar(total_sales_product.head(10), x='Product Name', y='Sales')
+fig=px.bar(total_sales_product.head(10), y='Product Name', x='Sales', orientation='h', color='Product Name')
 st.plotly_chart(fig, use_container_width=True)
